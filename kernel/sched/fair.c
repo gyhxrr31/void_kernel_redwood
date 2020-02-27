d93de6dc292cc15089d436ba83df80ab9aedc2e2
Vincent Guittot <vincent.guittot@linaro.org>
2020-02-27 16:41:15 +0100

    sched/fair: Fix runnable_avg for throttled cfs
    When a cfs_rq is throttled, its group entity is dequeued and its running
    tasks are removed. We must update runnable_avg with the old h_nr_running
    and update group_se->runnable_weight with the new h_nr_running at each
    level of the hierarchy.
    
    Reviewed-by: Ben Segall <bsegall@google.com>
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Fixes: 9f68395333ad ("sched/pelt: Add a new runnable average signal")
    Link: https://lkml.kernel.org/r/20200227154115.8332-1-vincent.guittot@linaro.org
    [Tashar02: Adapt to commit 294de8933adbd on msm-5.4]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4649,8 +4649,13 @@ static bool throttle_cfs_rq(struct cfs_rq *cfs_rq)
 		if (!se->on_rq)
 			break;
 
-		if (dequeue)
+		if (dequeue) {
 			dequeue_entity(qcfs_rq, se, DEQUEUE_SLEEP);
+		} else {
+			update_load_avg(qcfs_rq, se, 0);
+			se_update_runnable(se);
+		}
+
 		qcfs_rq->h_nr_running -= task_delta;
 		qcfs_rq->idle_h_nr_running -= idle_task_delta;
 
@@ -4700,7 +4705,8 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 		if (se->on_rq)
 			break;
 		cfs_rq = cfs_rq_of(se);
-		enqueue_entity(cfs_rq, se, ENQUEUE_WAKEUP);
+		update_load_avg(cfs_rq, se, 0);
+		se_update_runnable(se);
 
 		cfs_rq->h_nr_running += task_delta;
 		cfs_rq->idle_h_nr_running += idle_task_delta;
