6c2ef99d0c83fe5c9d276e7f277d4f1150bc861c
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2020-09-12 14:35:10 +0530

    mi_thermal_interface: Workaround for xiaomi thermal mitigation
    For some reason, leaving this unconfigured is making the charger voltage
    become low and it's affecting UX. Set this to Evaluation by default to
    improve UX. Also while on it, convert deprecated simple_strtol to
    kstrtoint.
    
    Bug: Slow Charging
    Test: Charging speed normal, sconfig at 11 as defined
    
    Change-Id: Id974ceaf31da0439502372a09ff6b5f4ef2982d8
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/thermal/mi_thermal_interface.c
+++ drivers/thermal/mi_thermal_interface.c
@@ -68,7 +68,7 @@ static atomic_t market_download_limit = ATOMIC_INIT(0);
 static atomic_t modem_limit = ATOMIC_INIT(0);
 static atomic_t poor_modem_limit = ATOMIC_INIT(0);
 static atomic_t temp_state = ATOMIC_INIT(0);
-static atomic_t sconfig = ATOMIC_INIT(-1);
+static atomic_t sconfig = ATOMIC_INIT(10);
 static atomic_t wifi_limit = ATOMIC_INIT(0);
 
 static const char *board_sensor;
@@ -435,10 +435,12 @@ static ssize_t thermal_sconfig_store(struct device *dev,
 				     struct device_attribute *attr,
 				     const char *buf, size_t len)
 {
-	int val = -1;
-	val = simple_strtol(buf, NULL, 10);
+	int ret, val = -1;
+	ret = kstrtoint(buf, 10, &val);
 	atomic_set(&sconfig, val);
 
+	if (ret)
+		return ret;
 	return len;
 }
 
@@ -462,10 +464,12 @@ static ssize_t thermal_temp_state_store(struct device *dev,
 					struct device_attribute *attr,
 					const char *buf, size_t len)
 {
-	int val = -1;
-	val = simple_strtol(buf, NULL, 10);
+	int ret, val = -1;
+	ret = kstrtoint(buf, 10, &val);
 	atomic_set(&temp_state, val);
 
+	if (ret)
+		return ret;
 	return len;
 }
 
