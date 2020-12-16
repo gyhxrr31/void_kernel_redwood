3744cb3d0b55ee97f3fbed73dc1d35f159ccf274
Zhaoyang Huang <huangzhaoyang@gmail.com>
2020-12-15 20:42:23 -0800

    mm: fix a race on nr_swap_pages
    The scenario on which "Free swap = -4kB" happens in my system, which is caused
    by several get_swap_pages racing with each other and show_swap_cache_info
    happens simutaniously. No need to add a lock on get_swap_page_of_type as we
    remove "Presub/PosAdd" here.
    
    ProcessA			ProcessB			ProcessC
    ngoals = 1			ngoals = 1
    avail = nr_swap_pages(1)	avail = nr_swap_pages(1)
    nr_swap_pages(1) -= ngoals
    				nr_swap_pages(0) -= ngoals
    								nr_swap_pages = -1
    
    Link: https://lkml.kernel.org/r/1607050340-4535-1-git-send-email-zhaoyang.huang@unisoc.com
    Signed-off-by: Zhaoyang Huang <zhaoyang.huang@unisoc.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1016,9 +1016,13 @@ int get_swap_pages(int n_goal, swp_entry_t swp_entries[], int entry_size)
 	/* Only single cluster request supported */
 	WARN_ON_ONCE(n_goal > 1 && size == SWAPFILE_CLUSTER);
 
+	spin_lock(&swap_avail_lock);
+
 	avail_pgs = atomic_long_read(&nr_swap_pages) / size;
-	if (avail_pgs <= 0)
+	if (avail_pgs <= 0) {
+		spin_unlock(&swap_avail_lock);
 		goto noswap;
+	}
 
 	if (n_goal > SWAP_BATCH)
 		n_goal = SWAP_BATCH;
@@ -1027,8 +1031,6 @@ int get_swap_pages(int n_goal, swp_entry_t swp_entries[], int entry_size)
 		n_goal = avail_pgs;
 
 	atomic_long_sub(n_goal * size, &nr_swap_pages);
-
-	spin_lock(&swap_avail_lock);
 
 start_over:
 	node = numa_node_id();
@@ -1104,14 +1106,13 @@ swp_entry_t get_swap_page_of_type(int type)
 
 	spin_lock(&si->lock);
 	if (si->flags & SWP_WRITEOK) {
-		atomic_long_dec(&nr_swap_pages);
 		/* This is called for allocating swap entry, not cache */
 		offset = scan_swap_map(si, 1);
 		if (offset) {
+			atomic_long_dec(&nr_swap_pages);
 			spin_unlock(&si->lock);
 			return swp_entry(type, offset);
 		}
-		atomic_long_inc(&nr_swap_pages);
 	}
 	spin_unlock(&si->lock);
 fail:
