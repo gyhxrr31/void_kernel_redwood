4078721b5b4d5ec4eaa8661e7e11151e6b823a7b
Juhyung Park <qkrwngud825@gmail.com>
2021-09-17 22:06:28 +0900

    Revert "ARM: gic-v3: Log the IRQs in RTB before handling an IRQ"
    This reverts commit 23c2a9291f25e5ee26f9bdb1df8d50d5e8a411c9.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -18,8 +18,6 @@
 #include <linux/percpu.h>
 #include <linux/refcount.h>
 #include <linux/slab.h>
-#include <linux/msm_rtb.h>
-
 #include <linux/irqchip.h>
 #include <linux/irqchip/arm-gic-common.h>
 #include <linux/irqchip/arm-gic-v3.h>
@@ -648,7 +646,6 @@ static asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs
 	if (likely(irqnr > 15)) {
 		int err;
 
-		uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 		if (static_branch_likely(&supports_deactivate_key))
 			gic_write_eoir(irqnr);
 		else
@@ -662,7 +659,6 @@ static asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs
 		return;
 	}
 	if (irqnr < 16) {
-		uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 		gic_write_eoir(irqnr);
 		if (static_branch_likely(&supports_deactivate_key))
 			gic_write_dir(irqnr);
