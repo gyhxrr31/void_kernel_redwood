9f49096d4e098cee6c552c2c7497e71f24ac2f51
Rituraj Raman <riturama@codeaurora.org>
2021-11-23 14:36:11 +0530

    msm: kgsl: set/clear power_flags
    For gmu/rgmu based targets, set/clear power_flags during
    clock enable/disable. This flag is checked before accessing
    power counter.
    
    Change-Id: Ie3eba37cc66135d909daa2669fd8e7e0fe4287a4
    Signed-off-by: Rituraj Raman <riturama@codeaurora.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno_a6xx_gmu.c
+++ drivers/gpu/msm/adreno_a6xx_gmu.c
@@ -1750,12 +1750,14 @@ void a6xx_gmu_suspend(struct adreno_device *adreno_dev)
 {
 	struct a6xx_gmu_device *gmu = to_a6xx_gmu(adreno_dev);
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
+	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
 
 	a6xx_gmu_irq_disable(adreno_dev);
 
 	a6xx_gmu_pwrctrl_suspend(adreno_dev);
 
 	clk_bulk_disable_unprepare(gmu->num_clks, gmu->clks);
+	clear_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags);
 
 	a6xx_cx_regulator_disable_wait(gmu->cx_gdsc, device, 5000);
 
@@ -2116,6 +2118,7 @@ int a6xx_gmu_enable_clks(struct adreno_device *adreno_dev)
 {
 	struct a6xx_gmu_device *gmu = to_a6xx_gmu(adreno_dev);
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
+	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
 	int ret;
 
 	a6xx_rdpm_cx_freq_update(gmu, GMU_FREQUENCY / 1000);
@@ -2139,6 +2142,7 @@ int a6xx_gmu_enable_clks(struct adreno_device *adreno_dev)
 	}
 
 	device->state = KGSL_STATE_AWARE;
+	set_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags);
 
 	return 0;
 }
@@ -2233,6 +2237,7 @@ err:
 
 clks_gdsc_off:
 	clk_bulk_disable_unprepare(gmu->num_clks, gmu->clks);
+	clear_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags);
 
 gdsc_off:
 	/* Poll to make sure that the CX is off */
@@ -2247,6 +2252,7 @@ static int a6xx_gmu_boot(struct adreno_device *adreno_dev)
 {
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
 	struct a6xx_gmu_device *gmu = to_a6xx_gmu(adreno_dev);
+	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
 	int ret = 0;
 
 	trace_kgsl_pwr_request_state(device, KGSL_STATE_AWARE);
@@ -2313,6 +2319,7 @@ err:
 
 clks_gdsc_off:
 	clk_bulk_disable_unprepare(gmu->num_clks, gmu->clks);
+	clear_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags);
 
 gdsc_off:
 	/* Poll to make sure that the CX is off */
@@ -2764,6 +2771,7 @@ static int a6xx_gmu_power_off(struct adreno_device *adreno_dev)
 {
 	struct a6xx_gmu_device *gmu = to_a6xx_gmu(adreno_dev);
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
+	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
 	int ret = 0;
 
 	if (device->gmu_fault)
@@ -2798,6 +2806,7 @@ static int a6xx_gmu_power_off(struct adreno_device *adreno_dev)
 	a6xx_hfi_stop(adreno_dev);
 
 	clk_bulk_disable_unprepare(gmu->num_clks, gmu->clks);
+	clear_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags);
 
 	/* Poll to make sure that the CX is off */
 	a6xx_cx_regulator_disable_wait(gmu->cx_gdsc, device, 5000);
