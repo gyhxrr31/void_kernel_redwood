d391e894c3fd7eda28a3a23d65a78ec96f43be3f
Zhen Lei <thunder.leizhen@huawei.com>
2022-08-04 10:34:19 +0800

    sched/debug: Try trigger_single_cpu_backtrace(cpu) in dump_cpu_task()
    The trigger_all_cpu_backtrace() function attempts to send an NMI to the
    target CPU, which usually provides much better stack traces than the
    dump_cpu_task() function's approach of dumping that stack from some other
    CPU.  So much so that most calls to dump_cpu_task() only happen after
    a call to trigger_all_cpu_backtrace() has failed.  And the exception to
    this rule really should attempt to use trigger_all_cpu_backtrace() first.
    
    Therefore, move the trigger_all_cpu_backtrace() invocation into
    dump_cpu_task().
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/rcu/tree_stall.h
+++ kernel/rcu/tree_stall.h
@@ -253,8 +253,7 @@ static void rcu_dump_cpu_stacks(void)
 		raw_spin_lock_irqsave_rcu_node(rnp, flags);
 		for_each_leaf_node_possible_cpu(rnp, cpu)
 			if (rnp->qsmask & leaf_node_cpu_bit(rnp, cpu))
-				if (!trigger_single_cpu_backtrace(cpu))
-					dump_cpu_task(cpu);
+				dump_cpu_task(cpu);
 		raw_spin_unlock_irqrestore_rcu_node(rnp, flags);
 	}
 }
