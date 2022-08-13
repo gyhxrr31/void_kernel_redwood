1eb38c6efb26cff88f49d42b2ea5d39837a586ef
Kazuki Hashimoto <kazukih@tuta.io>
2022-08-13 15:56:11 +0900

    lpm-levels: Remove cpu_lock
    These bits of code are never going to run concurrently as confirmed by
    runtime tests.
    
    Change-Id: Ib3f77458e489eaa50160fa7c7349d67e3736d23a
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -228,13 +228,11 @@ static void disable_rimps_timer(struct lpm_cpu *cpu)
 	if (!cpu->rimps_tmr_base)
 		return;
 
-	spin_lock(&cpu->cpu_lock);
 	ctrl_val = readl_relaxed(cpu->rimps_tmr_base + TIMER_CTRL);
 	writel_relaxed(ctrl_val & ~(TIMER_CONTROL_EN),
 				cpu->rimps_tmr_base + TIMER_CTRL);
 	/* Ensure the write is complete before returning. */
 	wmb();
-	spin_unlock(&cpu->cpu_lock);
 
 }
 
@@ -257,7 +255,6 @@ static void program_rimps_timer(struct lpm_cpu *cpu)
 		return;
 
 	next_event = us_to_ticks(next_event);
-	spin_lock(&cpu->cpu_lock);
 
 	/* RIMPS timer pending should be read before programming timeout val */
 	readl(cpu->rimps_tmr_base + TIMER_PENDING);
@@ -269,7 +266,6 @@ static void program_rimps_timer(struct lpm_cpu *cpu)
 				cpu->rimps_tmr_base + TIMER_CTRL);
 	/* Ensure the write is complete before returning. */
 	wmb();
-	spin_unlock(&cpu->cpu_lock);
 }
 
 #ifdef CONFIG_SMP
