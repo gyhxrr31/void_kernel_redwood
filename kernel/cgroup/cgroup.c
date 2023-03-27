389e773556ac69ab9705dcff165e861218dcdb59
Sudarshan Rajagopalan <quic_sudaraja@quicinc.com>
2023-03-27 15:17:53 -0700

    FROMLIST: ANDROID: GKI: psi: remove 500ms min window size limitation for triggers
    Current 500ms min window size for psi triggers limits polling interval
    to 50ms to prevent polling threads from using too much cpu bandwidth by
    polling too frequently. However the number of cgroups with triggers is
    unlimited, so this protection can be defeated by creating multiple
    cgroups with psi triggers (triggers in each cgroup are served by a single
    "psimon" kernel thread).
    Instead of limiting min polling period, which also limits the latency of
    psi events, it's better to limit psi trigger creation to authorized users
    only, like we do for system-wide psi triggers (/proc/pressure/* files can
    be written only by processes with CAP_SYS_RESOURCE capability). This also
    makes access rules for cgroup psi files consistent with system-wide ones.
    Add a CAP_SYS_RESOURCE capability check for cgroup psi file writers and
    remove the psi window min size limitation.
    
    Bug: 269247660
    Change-Id: I8876aa306cf2ba5acdf4daa5a5eff0665537bfeb
    Suggested-by: Sudarshan Rajagopalan <quic_sudaraja@quicinc.com>
    Link: https://lore.kernel.org/all/20230303011346.3342233-1-surenb@google.com/
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Sudarshan Rajagopalan <quic_sudaraja@quicinc.com>
    Signed-off-by: Georgi Djakov <quic_c_gdjako@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -3834,6 +3834,12 @@ static __poll_t cgroup_pressure_poll(struct kernfs_open_file *of,
 	return psi_trigger_poll(&ctx->psi.trigger, of->file, pt);
 }
 
+static int cgroup_pressure_open(struct kernfs_open_file *of)
+{
+	return (of->file->f_mode & FMODE_WRITE && !capable(CAP_SYS_RESOURCE)) ?
+		-EPERM : 0;
+}
+
 static void cgroup_pressure_release(struct kernfs_open_file *of)
 {
 	struct cgroup_file_ctx *ctx = of->priv;
@@ -5138,6 +5144,7 @@ static struct cftype cgroup_base_files[] = {
 	{
 		.name = "io.pressure",
 		.flags = CFTYPE_PRESSURE,
+		.open = cgroup_pressure_open,
 		.seq_show = cgroup_io_pressure_show,
 		.write = cgroup_io_pressure_write,
 		.poll = cgroup_pressure_poll,
@@ -5146,6 +5153,7 @@ static struct cftype cgroup_base_files[] = {
 	{
 		.name = "memory.pressure",
 		.flags = CFTYPE_PRESSURE,
+		.open = cgroup_pressure_open,
 		.seq_show = cgroup_memory_pressure_show,
 		.write = cgroup_memory_pressure_write,
 		.poll = cgroup_pressure_poll,
@@ -5154,6 +5162,7 @@ static struct cftype cgroup_base_files[] = {
 	{
 		.name = "cpu.pressure",
 		.flags = CFTYPE_PRESSURE,
+		.open = cgroup_pressure_open,
 		.seq_show = cgroup_cpu_pressure_show,
 		.write = cgroup_cpu_pressure_write,
 		.poll = cgroup_pressure_poll,
@@ -5163,6 +5172,7 @@ static struct cftype cgroup_base_files[] = {
 	{
 		.name = "irq.pressure",
 		.flags = CFTYPE_PRESSURE,
+		.open = cgroup_pressure_open,
 		.seq_show = cgroup_irq_pressure_show,
 		.write = cgroup_irq_pressure_write,
 		.poll = cgroup_pressure_poll,
