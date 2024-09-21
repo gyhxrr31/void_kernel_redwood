0636b37839b2ba99dd5f65f30ad306b384882837
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-09-22 02:21:56 +0600

    techpack: touch: focaltech_3680: Convert to "struct proc_ops"
    We recently converted to struct proc_ops. Utilize that.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/touch/focaltech_3680/focaltech_test/focaltech_test.c
+++ techpack/touch/focaltech_3680/focaltech_test/focaltech_test.c
@@ -2373,11 +2373,11 @@ int tp_selftest_release(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static const struct file_operations tp_selftest_fops = {
-	.open = tp_selftest_open,
-	.read = tp_selftest_read,
-	.write = tp_selftest_write,
-	.release = tp_selftest_release,
+static const struct proc_ops tp_selftest_fops = {
+	.proc_open = tp_selftest_open,
+	.proc_read = tp_selftest_read,
+	.proc_write = tp_selftest_write,
+	.proc_release = tp_selftest_release,
 };
 
 static int32_t datadump_show(struct seq_file *m, void *v)
@@ -2498,12 +2498,11 @@ static int32_t tp_datadump_open(struct inode *inode, struct file *file)
 	return seq_open(file, &tp_datadump_seq_ops);
 }
 
-static const struct file_operations tp_datadump_fops = {
-	.owner = THIS_MODULE,
-	.open = tp_datadump_open,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = seq_release,
+static const struct proc_ops tp_datadump_fops = {
+	.proc_open = tp_datadump_open,
+	.proc_read =seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = seq_release,
 };
 
 static int fts_test_func_init(struct fts_ts_data *ts_data)
