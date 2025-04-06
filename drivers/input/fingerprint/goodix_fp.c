671b634093dd277abd265b8b4aa4cd7df75ff0b4
EmanuelCN <emanuelghub@gmail.com>
2025-04-06 17:15:07 +0300

    fingerprint: goodix: Correct regulator configuration
    Automatically detect the regulator and also set correct load for
    redwood.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/fingerprint/goodix_fp.c
+++ drivers/input/fingerprint/goodix_fp.c
@@ -109,8 +109,10 @@ static inline void gf_setup(struct gf_dev *gf_dev)
 				 IRQF_TRIGGER_RISING | IRQF_ONESHOT, "gf",
 				 gf_dev))
 		irq_switch(gf_dev, 1);
-	gf_dev->vreg = regulator_get(NULL, "pm8150_l17");
-	if (!regulator_is_enabled(gf_dev->vreg)) {
+	gf_dev->vreg = regulator_get(&gf_dev->spi->dev, "l3c_vdd");
+	if (regulator_is_enabled(gf_dev->vreg)) {
+		regulator_set_load(gf_dev->vreg, 200000);
+	} else if (!regulator_is_enabled(gf_dev->vreg)) {
 		regulator_set_voltage(gf_dev->vreg, 3000000, 3000000);
 		if (regulator_enable(gf_dev->vreg))
 			return;
