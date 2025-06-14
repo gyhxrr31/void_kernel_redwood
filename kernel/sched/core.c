386c79ec9c73ddf02c46f828a2d9e26c6221cfb7
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-06-14 18:39:36 +0600

    [FIXUP] sched/core: Use task_struct::__state for locked-down task state sampling
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4093,7 +4093,7 @@ int task_call_func(struct task_struct *p, task_call_f func, void *arg)
 
 	raw_spin_lock_irqsave(&p->pi_lock, rf.flags);
 
-	state = p->__state;
+	state = READ_ONCE(p->__state);
 
 	/*
 	 * Ensure we load p->on_rq after p->__state, otherwise it would be
